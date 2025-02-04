680129cfe576d9ddf89564b8fcfac273d888d73d
aatack <alexanderjtatack@gmail.com>
2025-02-04 02:04:59 +0000

    Figure out how to borrow a mutable value

--- backend-v2/src/store.rs
+++ backend-v2/src/store.rs
@@ -1,11 +1,14 @@
 use json::JsonValue;
 use rusqlite::Connection;
-use std::{cell::OnceCell, time::SystemTime};
+use std::{
+    cell::{OnceCell, RefCell},
+    time::SystemTime,
+};
 use uuid::Uuid;
 
 pub struct Store {
     path: String,
-    connection_cell: OnceCell<Connection>,
+    connection_cell: OnceCell<RefCell<Connection>>,
 }
 
 pub struct StoreEntity {
@@ -29,68 +32,78 @@ impl Store {
         }
     }
 
-    pub fn connection(&self) -> &Connection {
-        self.connection_cell.get_or_init(|| {
-            let connection = Connection::open(self.path.clone()).unwrap();
+    pub fn write_entities(&self, entities: &[StoreEntity]) {
+        let mut connection = self.connection();
+        let transaction = connection.transaction().unwrap();
+    }
 
-            // Set up the entities table
-            connection
-                .execute(
-                    "create table if not exists entities (
-                        timestamp integer not null,
-                        entity text not null,
-                        key text not null,
-                        value text not null
-                    );",
-                    [],
+    pub fn connection(&self) -> std::cell::RefMut<Connection> {
+        let connection = self
+            .connection_cell
+            .get_or_init(|| {
+                RefCell::new(
+                    Connection::open(&self.path).expect("Failed to open database"),
                 )
-                .unwrap();
+            })
+            .borrow_mut();
 
-            connection
-                .execute(
-                    "create index if not exists idx_entities_timestamp
-                        on entities (timestamp)",
-                    [],
-                )
-                .unwrap();
+        // Set up the entities table
+        connection
+            .execute(
+                "create table if not exists entities (
+                    timestamp integer not null,
+                    entity text not null,
+                    key text not null,
+                    value text not null
+                );",
+                [],
+            )
+            .unwrap();
 
-            connection
-                .execute(
-                    "create index if not exists idx_entities_entity
-                        on entities (entity)",
-                    [],
-                )
-                .unwrap();
+        connection
+            .execute(
+                "create index if not exists idx_entities_timestamp
+                    on entities (timestamp)",
+                [],
+            )
+            .unwrap();
 
-            // Set up the resources table
-            connection
-                .execute(
-                    "create table if not exists resources (
-                        timestamp text not null,
-                        resource text not null,
-                        data blob not null
-                    );",
-                    [],
-                )
-                .unwrap();
+        connection
+            .execute(
+                "create index if not exists idx_entities_entity
+                    on entities (entity)",
+                [],
+            )
+            .unwrap();
 
-            connection
-                .execute(
-                    "create index if not exists idx_resources_timestamp
-                            on resources (timestamp)",
-                    [],
-                )
-                .unwrap();
+        // Set up the resources table
+        connection
+            .execute(
+                "create table if not exists resources (
+                    timestamp text not null,
+                    resource text not null,
+                    data blob not null
+                );",
+                [],
+            )
+            .unwrap();
 
-            connection
-                .execute(
-                    "create index if not exists idx_resources_resource
-                            on resources (resource)",
-                    [],
-                )
-                .unwrap();
+        connection
+            .execute(
+                "create index if not exists idx_resources_timestamp
+                    on resources (timestamp)",
+                [],
+            )
+            .unwrap();
 
-            connection
-        })
+        connection
+            .execute(
+                "create index if not exists idx_resources_resource
+                    on resources (resource)",
+                [],
+            )
+            .unwrap();
+
+        connection
     }
 }
