61417a0ee7ad48977265388d951011f9c87c73b3
aatack <alexanderjtatack@gmail.com>
2025-04-02 00:01:32 +0100

    Ditto for resources

--- backend-v2/store.py
+++ backend-v2/store.py
@@ -12,28 +12,50 @@ class Store:
         self.path.parent.mkdir(parents=True, exist_ok=True)
         cursor = sqlite3.connect(self.path)
 
+        # Entities
         cursor.execute(
             """
             create table if not exists entities (
                 timestamp integer not null,
-                entity text not null,
+                uuid text not null,
                 key text not null,
                 value text not null
             );
             """
         )
-
         cursor.execute(
             """
             create index if not exists idx_entities_timestamp
                 on entities (timestamp)
             """
         )
-
         cursor.execute(
             """
-            create index if not exists idx_entities_entity
-                on entities (entity)
+            create index if not exists idx_entities_uuid
+                on entities (uuid)
+            """
+        )
+
+        # Resources
+        cursor.execute(
+            """
+            create table if not exists resources (
+                timestamp text not null,
+                uuid text not null,
+                data blob not null
+            )
+            """
+        )
+        cursor.execute(
+            """
+            create index if not exists idx_resources_timestamp
+                on resources (timestamp)
+            """
+        )
+        cursor.execute(
+            """
+            create index if not exists idx_resources_uuid
+                on resources (uuid)
             """
         )
 
