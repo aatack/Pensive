4165be0550b16c2399bb094ca47d0dc9cbf4547f
aatack <alexanderjtatack@gmail.com>
2025-04-01 23:59:34 +0100

    Create the entities table

--- backend-v2/store.py
+++ backend-v2/store.py
@@ -10,4 +10,31 @@ class Store:
     @cached_property
     def cursor(self) -> sqlite3.Connection:
         self.path.parent.mkdir(parents=True, exist_ok=True)
-        return sqlite3.connect(self.path)
+        cursor = sqlite3.connect(self.path)
+
+        cursor.execute(
+            """
+            create table if not exists entities (
+                timestamp integer not null,
+                entity text not null,
+                key text not null,
+                value text not null
+            );
+            """
+        )
+
+        cursor.execute(
+            """
+            create index if not exists idx_entities_timestamp
+                on entities (timestamp)
+            """
+        )
+
+        cursor.execute(
+            """
+            create index if not exists idx_entities_entity
+                on entities (entity)
+            """
+        )
+
+        return cursor
