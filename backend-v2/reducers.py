456cad8cf0d4546b7772660850e64b95beed8b3e
aatack <alexanderjtatack@gmail.com>
2025-04-02 01:15:59 +0100

    Implement basic reducers

---
+++ backend-v2/reducers.py
@@ -0,0 +1,29 @@
+from helpers import Json
+
+
+def replace(_: Json, update: Json) -> Json:
+    return update
+
+
+def array(current: Json, update: Json) -> Json:
+    if not isinstance(update, str) or len(update) < 1:
+        return current
+
+    operation, text = update[0], update[1:]
+
+    if not isinstance(current, list):
+        current = []
+
+    present = text in current
+    if operation == "+" and not present:
+        return current + [text]
+    elif operation == "-" and present:
+        return [item for item in current if item != text]
+    elif (
+        operation == ">"
+        and present
+        and (index := current.index(text) < len(current) - 1)
+    ):
+        current[index], current[index + 1] = current[index + 1], current[index]
+    elif operation == "<" and present and (index := current.index(text) > 0):
+        current[index], current[index - 1] = current[index - 1], current[index]
