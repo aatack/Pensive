f7679907931ae679c197666a418550934ba166bd
aatack <alexanderjtatack@gmail.com>
2025-04-02 01:51:43 +0100

    Read entities from a client

--- backend-v2/client.py
+++ backend-v2/client.py
@@ -1,6 +1,9 @@
+from collections import defaultdict
 from collections.abc import Callable, Iterator
+from uuid import UUID
 
 from helpers import Json
+from reducers import replace
 from store import Store
 
 
@@ -19,3 +22,21 @@ class Client:
     def stores(self) -> Iterator[Store]:
         yield self.root_store
         yield from self.additional_stores
+
+    def root_entity(self) -> UUID | None:
+        return self.root_store.root_entity()
+
+    def read_entities(self, uuids: list[UUID]) -> dict[UUID, dict[str, Json]]:
+        entities: dict[UUID, dict[str, Json]] = defaultdict(
+            lambda: defaultdict(lambda: None)
+        )
+
+        for _, uuid, key, value in sorted(
+            (entity for store in self.stores for entity in store.read_entities(uuids)),
+            key=lambda entity: entity.timestamp,
+        ):
+            entities[uuid][key] = self.reducers.get(key, replace)(
+                entities[uuid][key], value
+            )
+
+        return entities
