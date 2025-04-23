5acb4a7fee0f780b07099c1a6f2bb9a566014ca9
aatack <alexanderjtatack@gmail.com>
2025-04-23 23:06:38 +0100

    Utilise a context manager for connections

--- backend_v2/pensive_backend/store.py
+++ backend_v2/pensive_backend/store.py
@@ -1,7 +1,8 @@
+from collections.abc import Iterator
+from contextlib import contextmanager
 import json
 import sqlite3
 from datetime import datetime, timezone
-from functools import cached_property
 from pathlib import Path
 from typing import NamedTuple
 from uuid import UUID
@@ -26,8 +27,18 @@ class Store:
     def __init__(self, path: str | Path) -> None:
         self.path = Path(path)
 
-    @cached_property
-    def connection(self) -> sqlite3.Connection:
+        _ = self.old_connection  # Initialise the database
+
+    @contextmanager
+    def connection(self) -> Iterator[sqlite3.Connection]:
+        connection = sqlite3.connect(self.path)
+        try:
+            yield connection
+        finally:
+            connection.close()
+
+    @property
+    def old_connection(self) -> sqlite3.Connection:
         self.path.parent.mkdir(parents=True, exist_ok=True)
         connection = sqlite3.connect(self.path)
 
@@ -84,78 +95,85 @@ class Store:
         if len(entities) == 0:
             return
 
-        self.connection.executemany(
-            "insert into entities (timestamp, uuid, key, value) values (?, ?, ?, ?)",
-            [
-                (timestamp_to_int(timestamp), uuid.hex, key, json.dumps(value))
-                for timestamp, uuid, key, value in entities
-            ],
-        )
-        self.connection.commit()
+        with self.connection() as connection:
+            connection.executemany(
+                "insert into entities (timestamp, uuid, key, value) values (?, ?, ?, ?)",
+                [
+                    (timestamp_to_int(timestamp), uuid.hex, key, json.dumps(value))
+                    for timestamp, uuid, key, value in entities
+                ],
+            )
+            connection.commit()
 
     def root_entity(self) -> UUID | None:
-        result = (
-            self.connection.cursor()
-            .execute(
-                """
-                select uuid from entities
-                    where key = 'text'
-                    order by timestamp asc, uuid asc
-                    limit 1
-                """
+        with self.connection() as connection:
+            result = (
+                connection.cursor()
+                .execute(
+                    """
+                    select uuid from entities
+                        where key = 'text'
+                        order by timestamp asc, uuid asc
+                        limit 1
+                    """
+                )
+                .fetchone()
             )
-            .fetchone()
-        )
-        return None if result is None else UUID(result[0])
+            return None if result is None else UUID(result[0])
 
     def read_entities(self, uuids: list[UUID]) -> list[StoreEntity]:
         uuids_string = ", ".join(f"'{uuid.hex}'" for uuid in uuids)
-        result = (
-            self.connection.cursor()
-            .execute(
-                f"""
-                select timestamp, uuid, key, value from entities
-                    where uuid in ({uuids_string})
-                    order by timestamp asc, uuid asc
-                """
+        with self.connection() as connection:
+            result = (
+                connection.cursor()
+                .execute(
+                    f"""
+                    select timestamp, uuid, key, value from entities
+                        where uuid in ({uuids_string})
+                        order by timestamp asc, uuid asc
+                    """
+                )
+                .fetchall()
             )
-            .fetchall()
-        )
-        return [
-            StoreEntity(int_to_timestamp(timestamp), UUID(uuid), key, json.loads(value))
-            for timestamp, uuid, key, value in result
-        ]
+            return [
+                StoreEntity(
+                    int_to_timestamp(timestamp), UUID(uuid), key, json.loads(value)
+                )
+                for timestamp, uuid, key, value in result
+            ]
 
     def write_resources(self, resources: list[StoreResource]) -> None:
         if len(resources) == 0:
             return
 
-        self.connection.executemany(
-            "insert into resources (timestamp, uuid, data) values (?, ?, ?)",
-            [
-                (timestamp_to_int(timestamp), uuid.hex, data)
-                for timestamp, uuid, data in resources
-            ],
-        )
-        self.connection.commit()
+        with self.connection() as connection:
+            connection.executemany(
+                "insert into resources (timestamp, uuid, data) values (?, ?, ?)",
+                [
+                    (timestamp_to_int(timestamp), uuid.hex, data)
+                    for timestamp, uuid, data in resources
+                ],
+            )
+            connection.commit()
 
     def read_resources(self, uuids: list[UUID]) -> list[StoreResource]:
         uuids_string = ", ".join(f"'{uuid.hex}'" for uuid in uuids)
-        result = (
-            self.connection.cursor()
-            .execute(
-                f"""
-                select timestamp, uuid, data from resources
-                    where uuid in ({uuids_string})
-                    order by timestamp asc, uuid asc
-                """
+        with self.connection() as connection:
+            result = (
+                connection.cursor()
+                .execute(
+                    f"""
+                    select timestamp, uuid, data from resources
+                        where uuid in ({uuids_string})
+                        order by timestamp asc, uuid asc
+                    """
+                )
+                .fetchall()
             )
-            .fetchall()
-        )
-        return [
-            StoreResource(int_to_timestamp(timestamp), UUID(uuid), data)
-            for timestamp, uuid, data in result
-        ]
+            return [
+                StoreResource(int_to_timestamp(timestamp), UUID(uuid), data)
+                for timestamp, uuid, data in result
+            ]
 
 
 def timestamp_to_int(timestamp: datetime) -> int:
