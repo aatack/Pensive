0d3f90b5b9518057e4b19a7fa8d4cfd713cc5915
aatack <alexanderjtatack@gmail.com>
2025-04-25 03:00:41 +0100

    Remove old backend and rust backend

--- backend-v2-rust/src/helpers.rs
+++
@@ -1,9 +0,0 @@
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
-
-pub fn timestamp_to_integer(timestamp: SystemTime) -> i64 {
-    i64::try_from(timestamp.duration_since(UNIX_EPOCH).unwrap().as_millis()).unwrap()
-}
-
-pub fn integer_to_timestamp(integer: i64) -> SystemTime {
-    UNIX_EPOCH + Duration::from_millis(u64::try_from(integer).unwrap())
-}
