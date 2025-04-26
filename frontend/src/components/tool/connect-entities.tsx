aa4397b81932e20b3472c244b0f99c80b1510df1
aatack <alexanderjtatack@gmail.com>
2025-04-26 01:08:18 +0100

    Define a function for applying connections

--- frontend/src/components/tool/connect-entities.tsx
+++ frontend/src/components/tool/connect-entities.tsx
@@ -1,7 +1,24 @@
+import { useWrite } from "../../context/hooks";
+
 export type ConnectEntities = {
   type: "connectEntities";
   kind: "create" | "remove";
 
   sourceUuid: string;
   destinationUuid: string;
+};
+
+export const useConnectEntities = () => {
+  const write = useWrite();
+
+  return ({ kind, sourceUuid, destinationUuid }: ConnectEntities) =>
+    write({
+      [sourceUuid]: {
+        outbound:
+          kind === "create" ? `+${destinationUuid}` : `-${destinationUuid}`,
+      },
+      [destinationUuid]: {
+        inbound: kind === "create" ? `+${sourceUuid}` : `-${sourceUuid}`,
+      },
+    });
 };
