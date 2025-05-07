23e93a5a80f91f7ef0e38cdf4df30bb4745b167f
aatack <alexanderjtatack@gmail.com>
2025-05-07 13:40:19 +0100

    Implement typescript reducers

---
+++ common/src/reducers.ts
@@ -0,0 +1,57 @@
+import { Json } from "./helpers";
+
+export const replace = (_: Json, update: Json): Json => update;
+
+export const array = (current: Json, update: Json): Json => {
+  if (Array.isArray(update)) {
+    if (update.length === 0) {
+      return current;
+    } else {
+      return array(array(current, update[0]!), update.slice(1));
+    }
+  }
+
+  if (typeof update !== "string" || update.length < 1) {
+    return current;
+  }
+
+  const operation = update[0];
+  const text = update.slice(1);
+
+  if (!Array.isArray(current)) {
+    current = [];
+  }
+
+  const present = current.includes(text);
+
+  if (operation === "+" && !present) {
+    return [...current, text];
+  } else if (operation === "-" && present) {
+    const items = current.filter((item: string) => item !== text);
+    return items.length === 0 ? null : items;
+  } else if (operation === ">" && present) {
+    const index = current.indexOf(text);
+    if (index >= 0 && index < current.length - 1) {
+      const left: Json = current[index]!;
+      const right: Json = current[index + 1]!;
+
+      current[index + 1] = left;
+      current[index] = right;
+
+      return current;
+    }
+  } else if (operation === "<" && present) {
+    const index = current.indexOf(text);
+    if (index > 0) {
+      const left: Json = current[index - 1]!;
+      const right: Json = current[index]!;
+
+      current[index] = left;
+      current[index - 1] = right;
+
+      return current;
+    }
+  }
+
+  return current;
+};
