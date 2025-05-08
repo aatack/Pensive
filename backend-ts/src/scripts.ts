0bdaaeabd85f20591e11779c858aa64d8854f560
aatack <alexanderjtatack@gmail.com>
2025-05-08 14:04:55 +0100

    Define script for migrating UUIDs specifically

--- backend-ts/src/scripts.ts
+++ backend-ts/src/scripts.ts
@@ -41,13 +41,13 @@ export const migrateStoreEntities = (
       const result = transform(original);
 
       if (result === null) {
-        console.info("Deleting:", original);
+        console.info("Deleting", original);
 
         if (options?.apply) {
           deleteStatement.run(row.timestamp, row.uuid, row.key);
         }
       } else if (!equal(original, result)) {
-        console.info("Updating:", original, result);
+        console.info("Updating", original, "-->", result);
 
         if (options?.apply) {
           updateStatement.run(
@@ -65,4 +65,51 @@ export const migrateStoreEntities = (
   });
 
   migrate();
+};
+
+export const migrateUuids = (
+  storePath: string,
+  options?: { apply: boolean }
+) => {
+  const stripDashes = (uuid: string) => {
+    const first = uuid[0];
+    const rest = uuid.slice(1);
+
+    return (
+      first +
+      rest
+        .toString()
+        .split("")
+        .filter((character) => character !== "-")
+        .join("")
+    );
+  };
+
+  migrateStoreEntities(
+    storePath,
+    (entity) => {
+      if (entity.key === "inbound" || entity.key === "outbound") {
+        const value: string[] =
+          typeof entity.value === "string"
+            ? [entity.value]
+            : entity.value == null
+            ? []
+            : (entity.value as string[]);
+        const remapped = value.map(stripDashes);
+
+        return {
+          ...entity,
+          value:
+            remapped.length === 0
+              ? null
+              : remapped.length === 1
+              ? remapped[0]!
+              : remapped,
+        };
+      } else {
+        return entity;
+      }
+    },
+    options
+  );
 };
