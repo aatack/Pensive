9b48070bc0a44e3d90ce29e5a46bc52e14df91a7
aatack <alexanderjtatack@gmail.com>
2025-05-20 21:15:45 +0100

    Improve styling

--- frontend/src/components/settings/edit-hotkeys.tsx
+++ frontend/src/components/settings/edit-hotkeys.tsx
@@ -2,40 +2,42 @@ import {
   Table,
   TableBody,
   TableCell,
-  TableContainer,
-  TableHead,
   TableRow,
-  Paper,
   TextField,
   Typography,
   Stack,
 } from "@mui/material";
 import { useProvided } from "../../providers/provider";
-import { defaultHotkeys } from "../../constants";
+import { colours, defaultHotkeys, font } from "../../constants";
 import { Hotkeys } from "../../providers/hotkeys";
 
 export const EditHotkeys = () => {
   const hotkeys = useProvided("hotkeys");
 
   return (
-    <Stack sx={{ overflowY: "scroll" }}>
+    <Stack sx={{ overflowY: "scroll", padding: 2 }}>
       <Table>
         <TableBody>
           {Object.keys(defaultHotkeys).map((hotkey) => (
             <TableRow key={hotkey}>
-              <TableCell>
+              <TableCell sx={{ margin: 0, padding: 0 }}>
                 <Typography variant="body1Monospace" fontWeight="bold">
                   {hotkey}
                 </Typography>
               </TableCell>
-              <TableCell>
+              <TableCell sx={{ margin: 0, padding: 0 }}>
                 <TextField
-                  variant="outlined"
+                  variant="standard"
                   size="small"
                   value={hotkeys.value[hotkey as keyof Hotkeys] ?? ""}
                   onChange={({ target: { value } }) =>
                     hotkeys.swap((current) => ({ ...current, [hotkey]: value }))
                   }
+                  inputProps={{ sx: { p: 0.5, color: colours.tx, ...font } }}
+                  InputProps={{
+                    disableUnderline: true,
+                    sx: { margin: 0, padding: 0 },
+                  }}
                 />
               </TableCell>
             </TableRow>
