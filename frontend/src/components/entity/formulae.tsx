a19fc118dcfc5ef179880d6e5eaba076f4a1bf2e
aatack <alexanderjtatack@gmail.com>
2025-08-10 17:08:34 +0100

    Catch exceptions before they crash the whole app

--- frontend/src/components/entity/formulae.tsx
+++ frontend/src/components/entity/formulae.tsx
@@ -1,7 +1,7 @@
 import { Divider, Typography } from "@mui/material";
 import { evaluate, parse } from "Parsle/src";
 import { serialise } from "Parsle/src/behaviours/serialise";
-import { formatStack } from "Parsle/src/models/result";
+import { formatStack, Result } from "Parsle/src/models/result";
 import { newScope } from "Parsle/src/types/scope";
 import { newString } from "Parsle/src/types/string";
 import { colours } from "../../constants";
@@ -9,20 +9,34 @@ import { colours } from "../../constants";
 export const FormulaEntity = ({ text }: { text: string }) => {
   const parseResult = parse(newString(text));
 
-  const result = parseResult.success
-    ? evaluate(parseResult.value, newScope([]))
-    : parseResult;
+  let result: Result;
+  try {
+    result = parseResult.success
+      ? evaluate(parseResult.value, newScope([]))
+      : parseResult;
+  } catch (e) {
+    result = { success: false, reason: e.toString(), stack: [] };
+  }
+
+  let formattedText: string;
+  try {
+    formattedText =
+      result.success === false ? formatStack(result) : serialise(result.value);
+  } catch (e) {
+    formattedText = e.toString();
+  }
 
   return (
     <>
       <Typography variant="body1Monospace" sx={{ whiteSpace: "pre-wrap" }}>
         {text}
       </Typography>
-      <Divider orientation="horizontal" sx={{ backgroundColor: colours.tx2, my: 0.5 }} />
+      <Divider
+        orientation="horizontal"
+        sx={{ backgroundColor: colours.tx2, my: 0.5 }}
+      />
       <Typography variant="body1Monospace" sx={{ whiteSpace: "pre-wrap" }}>
-        {result.success === false
-          ? formatStack(result)
-          : serialise(result.value)}
+        {formattedText}
       </Typography>
     </>
   );
