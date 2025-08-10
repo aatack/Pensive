f5ec7ccc6894de531f77343093ae41cb113d7e60
aatack <alexanderjtatack@gmail.com>
2025-08-10 16:04:09 +0100

    Evaluate the result in an emtpy scope

--- frontend/src/components/entity/formulae.tsx
+++ frontend/src/components/entity/formulae.tsx
@@ -1,18 +1,23 @@
-import { Button, Typography } from "@mui/material";
-import { parse } from "Parsle/src";
+import { Typography } from "@mui/material";
+import { evaluate, parse } from "Parsle/src";
 import { serialise } from "Parsle/src/behaviours/serialise";
 import { formatStack } from "Parsle/src/models/result";
+import { newScope } from "Parsle/src/types/scope";
 import { newString } from "Parsle/src/types/string";
 
 export const FormulaEntity = ({ text }: { text: string }) => {
   const parseResult = parse(newString(text));
 
+  const result = parseResult.success
+    ? evaluate(parseResult.value, newScope([]))
+    : parseResult;
+
   return (
     <>
       <Typography variant="body1Monospace" sx={{ whiteSpace: "pre-wrap" }}>
-        {parseResult.success === false
-          ? formatStack(parseResult)
-          : serialise(parseResult.value)}
+        {result.success === false
+          ? formatStack(result)
+          : serialise(result.value)}
       </Typography>
     </>
   );
