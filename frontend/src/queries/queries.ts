24143c6a9c55bb37b4eebeeef378d24c0fbc4bcf
aatack <alexanderjtatack@gmail.com>
2026-02-27 22:21:43 -0800

    Start resolving queries in a simpler manner

---
+++ frontend/src/queries/queries.ts
@@ -0,0 +1,82 @@
+import { EntityLinkKey, EntityState } from "../components/entity/entity";
+import { Mapping, mappingGet } from "../helpers/mapping";
+
+type Query = {
+  type: "links";
+  key: EntityLinkKey;
+};
+
+type ResolvedQuery = {
+  entityId: string;
+  entity: EntityState;
+
+  collapsed: boolean;
+
+  children: { key: string; value: ResolvedQuery }[];
+};
+
+const resolveQuery = (options: {
+  query: Query;
+  entityId: string;
+  collapsed: { [entityId: string]: boolean };
+  overrides: { [entityId: string]: Query | null };
+  lookup: Mapping<string, EntityState>;
+  path: string[];
+}): { data: ResolvedQuery; ids: string[] } => {
+  const entity = mappingGet(options.lookup, options.entityId);
+
+  // Always terminate here if the entity is collapsed or has already appeared in
+  // this path, unless it's the root
+  if (
+    (options.collapsed[options.entityId] && options.path.length > 0) ||
+    options.path.includes(options.entityId)
+  ) {
+    return {
+      data: {
+        entityId: options.entityId,
+        entity,
+        collapsed: true,
+        children: [],
+      },
+      ids: [options.entityId],
+    };
+  }
+
+  // If there is an override, return that instead
+  const override = options.overrides[options.entityId];
+  if (override != null) {
+    return resolveQuery({
+      ...options,
+      overrides: { ...options.overrides, [options.entityId]: null },
+      query: override,
+    });
+  }
+
+  switch (options.query.type) {
+    case "links": {
+      const key = options.query.key ?? "outbound";
+      const children = (entity[key] ?? []).map((id) =>
+        resolveQuery({
+          ...options,
+          entityId: id,
+          path: [...options.path, options.entityId],
+        }),
+      );
+
+      return {
+        data: {
+          entityId: options.entityId,
+          entity,
+          collapsed: false,
+          children: children.map((child) => ({
+            key: child.data.entityId,
+            value: child.data,
+          })),
+        },
+        ids: [options.entityId, ...children.flatMap((child) => child.ids)],
+      };
+    }
+  }
+
+  throw new Error("Not yet implemented");
+};
