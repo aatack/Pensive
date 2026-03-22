78571598d446d5a9714ccf961e0eec9460435396
aatack <alexanderjtatack@gmail.com>
2026-03-22 23:18:56 +0000

    Provision for actually running the integration

--- frontend/src/components/settings/integrations.ts
+++ frontend/src/components/settings/integrations.ts
@@ -1,3 +1,4 @@
+import { useHotkeys } from "react-hotkeys-hook/dist";
 import { usePersistentAtom } from "../../helpers/atoms";
 
 export type Integration = {
@@ -7,4 +8,16 @@ export type Integration = {
 
 export const useIntegrations = () => {
   return usePersistentAtom<Integration[]>("pensive-integrations", []);
+};
+
+export const useIntegration = (integration: Integration) => {
+  const content = {};
+
+  useHotkeys(integration.hotkey, () => {
+    fetch(integration.url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(content),
+    });
+  });
 };
