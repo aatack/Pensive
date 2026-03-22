9b9a233223e318ef2ab043ca194b9bba7d91799a
aatack <alexanderjtatack@gmail.com>
2026-03-22 23:04:16 +0000

    Scroll properly

--- frontend/src/components/settings/edit-hotkeys.tsx
+++ frontend/src/components/settings/edit-hotkeys.tsx
@@ -5,7 +5,6 @@ import {
   TableRow,
   TextField,
   Typography,
-  Stack,
 } from "@mui/material";
 import { useProvided } from "../../providers/use-provided";
 import { colours, defaultHotkeys, font } from "../../constants";
@@ -15,36 +14,34 @@ export const EditHotkeys = () => {
   const hotkeys = useProvided("hotkeys");
 
   return (
-    <Stack sx={{ overflowY: "scroll", padding: 2 }}>
-      <Table>
-        <TableBody>
-          {Object.keys(defaultHotkeys).map((hotkey) => (
-            <TableRow key={hotkey}>
-              <TableCell sx={{ margin: 0, padding: 0 }}>
-                <Typography variant="body1Monospace" fontWeight="bold">
-                  {hotkey}
-                </Typography>
-              </TableCell>
-              <TableCell sx={{ margin: 0, padding: 0 }}>
-                <TextField
-                  variant="standard"
-                  size="small"
-                  fullWidth
-                  value={hotkeys.value[hotkey as keyof Hotkeys] ?? ""}
-                  onChange={({ target: { value } }) =>
-                    hotkeys.swap((current) => ({ ...current, [hotkey]: value }))
-                  }
-                  inputProps={{ sx: { p: 0.5, color: colours.tx, ...font } }}
-                  InputProps={{
-                    disableUnderline: true,
-                    sx: { margin: 0, padding: 0 },
-                  }}
-                />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Stack>
+    <Table>
+      <TableBody>
+        {Object.keys(defaultHotkeys).map((hotkey) => (
+          <TableRow key={hotkey}>
+            <TableCell sx={{ margin: 0, padding: 0 }}>
+              <Typography variant="body1Monospace" fontWeight="bold">
+                {hotkey}
+              </Typography>
+            </TableCell>
+            <TableCell sx={{ margin: 0, padding: 0 }}>
+              <TextField
+                variant="standard"
+                size="small"
+                fullWidth
+                value={hotkeys.value[hotkey as keyof Hotkeys] ?? ""}
+                onChange={({ target: { value } }) =>
+                  hotkeys.swap((current) => ({ ...current, [hotkey]: value }))
+                }
+                inputProps={{ sx: { p: 0.5, color: colours.tx, ...font } }}
+                InputProps={{
+                  disableUnderline: true,
+                  sx: { margin: 0, padding: 0 },
+                }}
+              />
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 };
