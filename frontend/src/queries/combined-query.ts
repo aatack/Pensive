7e8712ee2877efe488a7a9cb93e6b0ed35fa726a
aatack <alexanderjtatack@gmail.com>
2026-04-07 00:20:44 +0100

    Start working on a combined query

---
+++ frontend/src/queries/combined-query.ts
@@ -0,0 +1,72 @@
+import { EntityState } from "../components/entity/entity";
+
+type Result = {
+  pivot: QueryFunction | null;
+  entityId: string;
+  entity: EntityState;
+
+  children: Result[];
+  complete: boolean;
+};
+
+type QueryFunction = {
+  children: (entity: EntityState) => string[];
+  pivot: (entity: EntityState) => QueryFunction | null;
+};
+
+const populateQuery = (
+  result: Result,
+  query: QueryFunction,
+  getEntity: (entityId: string) => EntityState,
+  pivots: { [entityId: string]: QueryFunction },
+) => {
+  // Note: reference equality is important in this function, as the pivots are
+  // stored as references and populated in-place later
+
+  const queue: { entityId: string; parent: Result }[] = query
+    .children(result.entity)
+    .map((entityId) => ({ entityId, parent: result }));
+  const pivotQueue: { result: Result; query: QueryFunction }[] = [];
+  let budget = 200;
+
+  // Explore initially
+  while (budget > 0) {
+    budget -= 1;
+    const item = queue.shift();
+    if (item == null) {
+      break;
+    }
+
+    const child: Result = {
+      pivot: null,
+      entityId: item.entityId,
+      entity: getEntity(item.entityId),
+      children: [],
+      complete: true,
+    };
+    item.parent.children.push(child);
+
+    const pivot = pivots[child.entityId] ?? query.pivot(child.entity) ?? null;
+    if (pivot == null) {
+      for (const childId of query.children(child.entity)) {
+        queue.push({ entityId: childId, parent: child });
+      }
+    } else {
+      pivotQueue.push({ result: child, query: pivot });
+    }
+  }
+
+  // Mark any unexplored items as incomplete
+  for (const item of queue) {
+    item.parent.complete = false;
+  }
+
+  // Prune the tree
+  const fixedResult = null;
+
+  // Populate any pivoted entities
+  for (const item of pivotQueue) {
+    item.result.pivot = item.query;
+    populateQuery(item.result, item.query, getEntity, pivots);
+  }
+};
